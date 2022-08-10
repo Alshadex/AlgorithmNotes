6842e904e665a435044464422bcc886ccd655080
Alex <linalex7@protonmail.com>
2022-08-09 23:56:17 -0400

    added target sum solution

---
+++ questions/TargetSum/targetsum.py
@@ -0,0 +1,15 @@
+
+nums = [4,2,1]
+target = 1
+
+dp = {}
+
+def foo(i, total):
+    if i == len(nums):
+        return 1 if total == target else 0
+    if (i,total) in dp:
+        return dp[(i,total)]
+    dp[(i,total)] = foo(i+1,total+nums[i]) + foo(i+1,total-nums[i])
+    return dp[(i,total)]
+
+print(foo(0,0))
